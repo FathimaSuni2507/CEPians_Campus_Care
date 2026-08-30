e550b7a20fb4b11a96278fc85266928d55c1497e
FathimaSuni2507 <fathimasuni67@gmail.com>
2026-08-30 11:57:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // 1. MongoDB Atlas Connection
